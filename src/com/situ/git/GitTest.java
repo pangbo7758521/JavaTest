dc1d6d5dbc3b986307dc1b6b77be40d717483ed6
pangbo7758521 <331252120@qq.com>
2017-09-29 19:20:52 -0700

    提交test1

--- src/com/situ/git/GitTest.java
+++ src/com/situ/git/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("GitTest.main()");
 	}
+	
+	public void test1() {
+		System.out.println("GitTest.test1()");
+	}
 }
